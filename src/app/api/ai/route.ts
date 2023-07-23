63cfcf6034e401b5ea67ebb0ed7afac308693dbf
eKoh951 <greatekoh@gmail.com>
2023-07-23 00:16:24 -0600

    Working serverless function

---
+++ src/app/api/ai/route.ts
@@ -0,0 +1,29 @@
+export const runtime = "edge";
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
+
+console.log("process.env.OPENAI_API_KEY", process.env.OPENAI_API_KEY);
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+export async function POST(request: NextRequest, res: NextResponse) {
+  const { messages } = await request.json();
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
+  });
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
